89c011d8a9a48001e3fa9a88d4d286fd96a17ff4
Aleksey <al.nkow@gmail.com>
2021-10-20 21:17:52 +0300

    fix scroll

--- src/components/Catalog/index.jsx
+++ src/components/Catalog/index.jsx
@@ -5,30 +5,34 @@ import Block from './Block';
 
 const Catalog = () => (
   <Wrap id="catalog">
-    <Block
-      id="rome"
-      title="Римская<br />штора"
-      description="Отличное дизайнерское решение для Вашего интерьера. Это тканевое полотно, которое при открытии создает волнообразные складки"
-      options={romeOptions}
-    />
-    <Block
-      id="tulle"
-      swap
-      title="Тюль"
-      options={tulleOptions}
-    />
-    <Block
-      id="portiere"
-      title="Портьеры"
-      description="Плотные портьеры хорошо держат форму, драпируются мягкими складками и смотрятся изыскано в любом интерьере"
-      options={portiereOptions}
-    />
-    <Block
-      id="cornice"
-      swap
-      title="Карнизы"
-      options={corniceOptions}
-    />
+    <div id="rome">
+      <Block
+        title="Римская<br />штора"
+        description="Отличное дизайнерское решение для Вашего интерьера. Это тканевое полотно, которое при открытии создает волнообразные складки"
+        options={romeOptions}
+      />
+    </div>
+    <div id="tulle">
+      <Block
+        swap
+        title="Тюль"
+        options={tulleOptions}
+      />
+    </div>
+    <div id="portiere">
+      <Block
+        title="Портьеры"
+        description="Плотные портьеры хорошо держат форму, драпируются мягкими складками и смотрятся изыскано в любом интерьере"
+        options={portiereOptions}
+      />
+    </div>
+    <div id="cornice">
+      <Block
+        swap
+        title="Карнизы"
+        options={corniceOptions}
+      />
+    </div>
   </Wrap>
 );
 
